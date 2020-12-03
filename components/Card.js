f61dd5e609e8408869cf487613adbe92083eb940
Michael Caveney <dylanesque27@gmail.com>
2020-12-03 01:22:39 -0500

    Refactored cards to use styled components

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,33 @@
 import { UpdateCharacter } from '../utils/graphql';
+
 import Card from '@material-ui/core/Card';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  middle: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  image: {
+    display: 'inline-block',
+    width: '100px',
+    height: '125px',
+  },
+  powerSelect: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingBottom: '1.5rem',
+  },
+});
 
 const CharCard = ({ character }) => {
+  const classes = useStyles();
   const {
     id,
     name,
@@ -38,35 +64,38 @@ const CharCard = ({ character }) => {
     setPowerThreeLevel(e.target.value);
   }
   return (
-    <Card key={id}>
+    <Card className={classes.root} key={id}>
       <p>{name}</p>
-      <img className="char-image" src={image} />
+      <div className={classes.middle}>
+        <img className={classes.image} src={image} />
+        <div className={classes.powerSelect}>
+          <label>Power One </label>
+          <select onChange={powerOneChange} value={powerOneLevel}>
+            {powerLevels.map((level) => {
+              return <option key={level}>{level}</option>;
+            })}
+          </select>
+          <label>Power Two </label>
+          <select onChange={powerTwoChange} value={powerTwoLevel}>
+            {powerLevels.map((level) => {
+              return <option key={level}>{level}</option>;
+            })}
+          </select>
+          <label>Power Three </label>
+          <select onChange={powerThreeChange} value={powerThreeLevel}>
+            {powerLevels.map((level) => {
+              return <option key={level}>{level}</option>;
+            })}
+          </select>
+        </div>
+      </div>
       <label>Character Level</label>
       <select onChange={nameChange} value={charLevel}>
         {charLevels.map((level) => {
           return <option key={level}>{level}</option>;
         })}
       </select>
-      <div className="power-select">
-        <label>Power One </label>
-        <select onChange={powerOneChange} value={powerOneLevel}>
-          {powerLevels.map((level) => {
-            return <option key={level}>{level}</option>;
-          })}
-        </select>
-        <label>Power Two </label>
-        <select onChange={powerTwoChange} value={powerTwoLevel}>
-          {powerLevels.map((level) => {
-            return <option key={level}>{level}</option>;
-          })}
-        </select>
-        <label>Power Three </label>
-        <select onChange={powerThreeChange} value={powerThreeLevel}>
-          {powerLevels.map((level) => {
-            return <option key={level}>{level}</option>;
-          })}
-        </select>
-      </div>
+
       <UpdateCharacter
         id={id}
         changes={{
