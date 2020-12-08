d34625e56580383df3ddbf3720bee41eec475c0f
Michael Caveney <dylanesque27@gmail.com>
2020-12-07 22:26:59 -0500

    Added styling to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,11 +2,15 @@ const Contact = () => {
   return (
     <>
       <h1>Contact Us</h1>
-      <p>
-        Found a bug you want to report or a feature you'd like to request? Do
-        you have feedback or other questions? Reach out to me at{' '}
-        <a href="mainto: michaelscaveney@gmail.com">michaelscaveney@gmail.com</a>
-      </p>
+      <main className="contact-main">
+        <p>
+          Found a bug you want to report or a feature you'd like to request? Do
+          you have feedback or other questions? Reach out to me at{' '}
+          <a href="mainto: michaelscaveney@gmail.com">
+            michaelscaveney@gmail.com
+          </a>
+        </p>
+      </main>
     </>
   );
 };
