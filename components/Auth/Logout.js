5ed20fe9adc27b251f8104ae19d3e14e7e8009b9
Michael Caveney <dylanesque27@gmail.com>
2020-12-08 22:48:20 -0500

    added reports feature to 2 star characters

--- components/Auth/Logout.js
+++ components/Auth/Logout.js
@@ -1,16 +1,30 @@
-import React from "react";
-import { Button } from "react-bootstrap";
-import Router from 'next/router'
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Router from 'next/router';
 
-const LogoutBtn = ({ logoutHandler }) => (
-  <Button
-    id="qsLogoutBtn"
-    variant="primary"
-    className="btn-margin loginBtn"
-    onClick={() => Router.push('/api/logout')}
-  >
-    Log Out
-  </Button>
-);
+const useStyles = makeStyles({
+  button: {
+    bottom: '0',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+});
+
+const LogoutBtn = ({ logoutHandler }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.button}>
+      <Button
+        id="qsLogoutBtn"
+        color="primary"
+        variant="contained"
+        onClick={() => Router.push('/api/logout')}
+      >
+        Log Out
+      </Button>
+    </div>
+  );
+};
 
 export default LogoutBtn;
--- components/Report.js
+++ components/Report.js
@@ -1,5 +1,6 @@
 import Card from '@material-ui/core/Card';
 
+
 const Report = ({ characters }) => {
   const activeCharacters = characters.filter(
     (character) =>
@@ -22,7 +23,29 @@ const Report = ({ characters }) => {
   const twoStars = activeCharacters
     .filter((character) => character.rarity === 2)
     .sort((a, b) => b.char_level - a.char_level);
-  // flag how far a character is from feedee covers
+
+
+  function getTwoStatus(level, feedee) {
+    if (level === 144) {
+      return 'Maximum Level'
+    } else {
+      if (level < 143 && level >= 139) {
+        return `${143 - level} levels to: Mighty Recruit Token`;
+      } else if (level < 139 && level >= 127) {
+        return `${139 - level} levels to: ${feedee} shards`;
+      } else if (level < 127 && level >= 119) {
+        return `${127 - level} levels to: ${feedee} shards`;
+      } else if (level < 119 && level >= 115) {
+        return `${119 - level} levels to: ${feedee} shards`;
+      } else if (level < 115 && level >= 107) {
+        return `${115 - level} levels to: ${feedee} shards`;
+      } else if (level < 107 && level >= 99) {
+        return `${107 - level} levels to: ${feedee} shards`;
+      } else {
+        return `${99 - level} levels to ${feedee} cover`;
+      }
+    }
+  }
   return (
     <>
       <h2>Five Star Characters</h2>
@@ -119,6 +142,7 @@ const Report = ({ characters }) => {
         }}
       >
         {twoStars.map((char) => {
+          console.log(char);
           return (
             <Card
               style={{
@@ -133,6 +157,8 @@ const Report = ({ characters }) => {
               <p>{char.name}</p>
               <img className="char-image" src={char.image} />
               <p>Level {char.char_level}</p>
+              <p>Status</p>
+              <p>{getTwoStatus(char.char_level, char.feedees)}</p>
             </Card>
           );
         })}
