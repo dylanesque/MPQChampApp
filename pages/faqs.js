ed677205ff43fa227776e321dd492c34c2b0555f
Michael Caveney <dylanesque27@gmail.com>
2020-12-14 14:51:16 -0500

    Updated .gitignore, built for production

--- pages/faqs.js
+++ pages/faqs.js
@@ -24,6 +24,7 @@ const FAQS = () => {
             <li>More realistic behavior in the character editor that maps more closely to the game. For example,
             you'll only be allowed to select the levels for a character that would be available per the number of covers they possess.</li>
             <li>More detailed information in roster reports</li>
+            <li>Reward Status for Five Star Characters</li>
           </ul>
         </main>
       </div>
