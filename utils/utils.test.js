1208cb6fba31c744da1589469d820dc32454531c
Michael Caveney <dylanesque27@gmail.com>
2021-01-17 21:31:24 -0500

    Unit tested more util functions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,7 +1,12 @@
-import { ExpansionPanelActions } from '@material-ui/core';
-import { getTwoStatus, parseFeeds } from './utils.js';
+import { getFourStatus, getThreeStatus, getTwoStatus, parseFeeds } from './utils.js';
 
 describe('feedees are corrctly parsed from', () => {
+   test('two star characters', () => {
+     expect(parseFeeds('Captain Marvel (Modern)')).toEqual([
+       'Captain Marvel (Modern)',
+       'Captain Marvel (Modern)',
+     ]);
+   });
   test('three star characters', () => {
     expect(
       parseFeeds('Jean Grey (All New X-Men),Jubilee (Uncanny X-Men)')
@@ -15,4 +20,21 @@ describe('next reward is calculated for', () => {
       '1 levels to: Mighty Recruit Token'
     );
   });
+   test('three star characters', () => {
+     expect(getThreeStatus(196, 'Jubilee (Uncanny X-Men)')).toEqual(
+       '4 level(s) to: Jubilee (Uncanny X-Men) shards'
+     );
+   });
+  
+   test('older four star characters', () => {
+     expect(getFourStatus(345, 'Rescue (Pepper Potts)')).toEqual(
+       '2 level(s) to: Rescue (Pepper Potts) shards'
+     );
+   });
+  
+   test('newer four star characters', () => {
+     expect(getFourStatus(331, 'Captain America (Infinity War)')).toEqual(
+       '4 level(s) to: Captain America (Infinity War) shards'
+     );
+   });
 })
