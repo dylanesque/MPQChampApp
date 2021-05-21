a30ea4302d5773ae07129fafa16e11a6e5d779da
Michael Caveney <dylanesque27@gmail.com>
2021-05-20 21:55:01 -0400

    Reopened site

--- components/Auth/Login.js
+++ components/Auth/Login.js
@@ -6,8 +6,7 @@ const Login = () => {
   return (
     <div className="login-page index-background">
       <h1>Welcome to MPQ Hall Of Champions!</h1>
-      <h2>We are currently down for database maintenance, but we'll be back soon!</h2>
-      {/*
+      <h2>Please login to continue</h2>
       <div>
         <Button
           id="qsLoginBtn"
@@ -20,7 +19,7 @@ const Login = () => {
           Log In
         </Button>
       </div>
-      */}
+
     </div>
   );
 };
