124f2369bd4125552b9e354e25a048038de29c65
Michael Caveney <dylanesque27@gmail.com>
2021-05-24 18:10:01 -0400

    began menu refactor

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,37 @@
 import styled from '@emotion/styled';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
 
 import NavBar from './NavBar';
 
 const Header = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
     <Nav>
+      <div className="mobile-menu">
+        <IconButton
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer variant="persistent" anchor="left" open={open} classes={{}}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Drawer>
+      </div>
       <NavBar />
     </Nav>
   );
